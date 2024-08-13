39826a6b217a260b9668f4eaeabe279077965c47
rmartins199 <rikardu.filipe@gmail.com>
2024-08-13 23:13:13 +0100

    Update front-end
    Foram feitas alterações para embelezar o front-end e correções mínimas nas funções existentes.

--- includes/collections.php
+++ includes/collections.php
@@ -5,29 +5,29 @@ require_once 'functions/collections.inc.php';
 <html>
 	<div class="container">
     	<div class="p-5 my-4 bg-light rounded-3">
-			<h2 class="pb-2 border-bottom">Comunidades e Colecções</h2>
+			<h2 class="pb-2 border-bottom h2title">Pesquisar por coleção</h2>
 			</br>
 			<?php 
 			foreach ($collection_results as $row):
 			?>
   				<ul class="list-group list-group-flush list-group-item">
-    				<li class="list-group-item"><?php echo htmlspecialchars($row['CollectionsName']); ?>
-					<span class="badge rounded-pill bg-primary float-end"><?php echo htmlspecialchars($row['Total']); ?></span>
+    				<li class="list-group-item"><a href="/?page=show_collections&id=<?php echo $row['collections_CollectionsID']; ?>" class="linktable"><?php echo htmlspecialchars($row['CollectionsName']); ?>
+					<span class="badge rounded-pill bg-secondary float-end"><?php echo htmlspecialchars($row['Total']); ?></span>
 					</li>
   				</ul>
 			<?php endforeach; ?>
 			</br>
 			<ul class="list-group list-group-horizontal">
 				<?php if ($page > 1): ?>
-  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $page - 1; ?>">Anterior</a>
+  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $page - 1; ?>" class="linktable">Anterior</a>
 					</li>
 				<?php endif; ?>
 				<?php for ($i = 1; $i <= $total_pages; $i++): ?>
-  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $i; ?>"><?php echo $i; ?></a>
+  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $i; ?>" class="linktable"><?php echo $i; ?></a>
 					</li>
 				<?php endfor; ?>
 				<?php if ($page < $total_pages): ?>
-  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $page + 1; ?>">Próxima</a>
+  				<li class="list-group-item"><a href="?page=collections&pg=<?php echo $page + 1; ?>" class="linktable">Próxima</a>
 					</li>
 				<?php endif; ?>
 			</ul>
--- includes/dateissued.php
+++ includes/dateissued.php
@@ -5,17 +5,16 @@ require_once 'functions/dateissued.inc.php';
 <html>
 <div class="container">
     <div class="p-5 my-4 bg-light rounded-3">	 
-	<h2 class="pb-2 border-bottom">Por data de publicação</h2>
-	</br>
+	<h2 class="pb-2 border-bottom h2title">Pesquisar por data</h2>
 	<div>   
-    	<table class="table table-striped table-condensed table-bordered">   
+    	<table class="table unstriped table-hover">   
         	<thead>
             	<tr>   
-            		<th width="15%">Data de publicação</th>
-					<th >Titulo</th>
-            		<th>Autor(a)</th>
-					<th>Tipo</th>
-					<th>Acesso</th>
+            		<th width="15%" scope="col">Data de publicação</th>
+					<th scope="col">Titulo</th>
+            		<th scope="col">Autor(a)</th>
+					<th scope="col">Tipo</th>
+					<th scope="col">Acesso</th>
             	</tr>   
           	</thead>   
      		<tbody>   
@@ -36,16 +35,20 @@ require_once 'functions/dateissued.inc.php';
            	?>
           	</tbody>   
         </table>
-		<div>
-        <?php if ($page > 1): ?>
-        	<a href="/?page=dateissued&pg=<?php echo $page - 1; ?>">Anterior</a>
-        <?php endif; ?>
-        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
-            <a href="/?page=dateissued&pg=<?php echo $i; ?>"><?php echo $i; ?></a>
-        <?php endfor; ?>
-        <?php if ($page < $total_pages): ?>
-            <a href="/?page=dateissued&pg=<?php echo $page + 1; ?>">Próxima</a>
-        <?php endif; ?>
+        <!-- Navegação de Paginação -->
+        <nav class="tableP">
+        	<ul class="pagination">
+        	<?php if ($page > 1): ?>
+        		<li class="page-item"><a href="/?page=dateissued&pg=<?php echo $page - 1; ?>" class="page-link">Anterior</a></li>
+        	<?php endif; ?>
+        	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
+            	<li class="page-item"><a href="/?page=dateissued&pg=<?php echo $i; ?>" class="page-link"><?php echo $i; ?></a></li>
+        	<?php endfor; ?>
+        	<?php if ($page < $total_pages): ?>
+            	<li class="page-item"><a href="/?page=dateissued&pg=<?php echo $page + 1; ?>" class="page-link">Próxima</a></li>
+        	<?php endif; ?>
+			</ul>
+        </nav>
     	</div>
 	</div>
 </div>
