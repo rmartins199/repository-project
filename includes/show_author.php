13b4df9b5db419c7d70e3264f03f666938a4c89e
rmartins199 <rikardu.filipe@gmail.com>
2024-08-18 02:06:34 +0100

    Correção de segurança V2
    O ID dos relatórios estavam visíveis em URL, para melhorar à segurança de dados sensíveis foi utilizado o método openssl_encrypt  para efetuar criptografia dos IDs com uma chave secreta e um vetor de inicialização (IV).
    
    A string base64 é decodificada e separada em dados criptografados e IV. Por fim recupero o ID original com o openssl_decrypt.

--- includes/show_author.php
+++ includes/show_author.php
@@ -1,6 +1,10 @@
 <?php
 // Conecta a ficheiros externos (por exemplo base dados)
 require_once 'functions/show_author.inc.php';
+
+// É defenida uma chave de encriptação segura
+$key = "xAHgjhu32bE%!Mop7u%Ae7g7%V6Pv6oC"; // A chave deve ter 16, 24 ou 32 caracteres (neste caso é de 32)
+$method = "aes-256-cbc";
 ?>
 <html>
     <div class="container">
@@ -41,9 +45,13 @@ require_once 'functions/show_author.inc.php';
                     <tbody>
                         <?php foreach ($results as $row): ?>
                             <?php $PubDate = (new DateTime($row['PublicationDate']))->format('Y-m-d'); ?>
+							<?php // Supondo que $document_id é o ID do documento
+									$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+									$encrypted_id = openssl_encrypt($row['DocumentId'], $method, $key, 0, $iv);
+									$encrypted_id = base64_encode($encrypted_id . '::' . $iv); ?>
                             <tr>
                                 <td><?php echo htmlspecialchars($PubDate); ?></td>
-                                <td><a href="/?page=publication&id=<?php echo $row['DocumentId']; ?>" class="linktable"><?php echo htmlspecialchars($row['DocumentTitle']); ?></a></td>
+                                <td><a href="/?page=publication&id=<?php echo urlencode($encrypted_id); ?>" class="linktable"><?php echo htmlspecialchars($row['DocumentTitle']); ?></a></td>
                                 <td><?php echo htmlspecialchars($row['DocumentSummary']); ?></td>
                                 <td><?php echo htmlspecialchars($row['CollectionsName']); ?></td>
                                 <td><?php echo htmlspecialchars($row['AccessName']); ?></td>
